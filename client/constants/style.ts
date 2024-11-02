bc405763681b7b1e994d7fbe52a03abd835d1024
edwinochieng <ochiengedwin440@gmail.com>
2024-11-03 01:08:31 +0300

    style: add page for shared styling resources

---
+++ client/constants/style.ts
@@ -0,0 +1,13 @@
+export const styles = {
+  card: "rounded-lg bg-[#FFFFFF] dark:bg-[#1A191E] p-4 shadow-lg dark:shadow-transparent",
+  screen: "flex-1 px-4 pt-4",
+  button:
+    "rounded-lg bg-[#006AF6] dark:bg-[#4B99FF] text-center text-[14px] text-[#F2F2F2] font-semibold uppercase",
+  heading: "text-[16px] font-semibold text-black dark:text-[#F2F2F2]",
+  subheading:
+    "text-[10px] text-[#828282] leading-[13px] text-center font-semibold uppercase dark:text-[#BDBDBD]",
+  amount:
+    "text-[#333333] text-[24px] leading-[44px] text-center font-semibold dark:text-[#F2F2F2]",
+  boldText: "text-[18px] font-semibold text-[#000000] dark:text-[#F2F2F2]",
+  subText: "text-[12px] font-normal text-[#000000] dark:text-[#BDBDBD]",
+};
