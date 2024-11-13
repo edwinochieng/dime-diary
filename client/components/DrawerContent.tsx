a51b3bc548fb373eda109557b4159e7d8584693f
edwinochieng <ochiengedwin440@gmail.com>
2024-11-14 01:43:31 +0300

    style: improve drawer styling

--- client/components/DrawerContent.tsx
+++ client/components/DrawerContent.tsx
@@ -1,29 +1,42 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, useColorScheme } from "react-native";
 import { DrawerContentScrollView } from "@react-navigation/drawer";
-import { useNavigation } from "expo-router";
+import { useNavigation, useRouter } from "expo-router";
 import { DrawerActions } from "@react-navigation/native";
 import { styles } from "@/constants/style";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function DrawerContent(props: any) {
   const navigation = useNavigation();
+  const colorScheme = useColorScheme();
+  const iconColor = colorScheme === "light" ? "#333333" : "#F2F2F2";
+  const router = useRouter();
 
   const handleLogout = () => {
     navigation.dispatch(DrawerActions.closeDrawer());
+    router.push("/login");
   };
 
   return (
     <DrawerContentScrollView {...props}>
-      <View className="flex-1">
-        <Text className="text-lg font-bold">John Doe</Text>
-        <Text className="text-gray-500">john.doe@example.com</Text>
-      </View>
+      <View className="flex-1 h-full p-4">
+        <View className="mt-5 flex-1 py-6 border border-t-transparent border-x-transparent border-b-[#000000] dark:border-b-[#F2F2F2]">
+          <Text className="text-[24px] font-semibold text-[#000000] dark:text-[#F2F2F2]">
+            Hello, Edwin!
+          </Text>
+        </View>
 
-      {/* Logout Button */}
-      <View className="">
-        <TouchableOpacity onPress={handleLogout} className={styles.button}>
-          <Text className={styles.boldText}>Logout</Text>
-        </TouchableOpacity>
+        <View className="mt-8">
+          <TouchableOpacity
+            onPress={handleLogout}
+            className="flex-row items-center"
+          >
+            <Feather name="log-out" size={22} color={iconColor} />
+            <Text className="ml-2 text-[20px] font-medium text-[#000000] dark:text-[#F2F2F2]">
+              Logout
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </DrawerContentScrollView>
   );
