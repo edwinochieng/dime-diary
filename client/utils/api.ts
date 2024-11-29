5f25fb712c557c9ad56b49b8eb59f18dd531d217
edwinochieng <ochiengedwin440@gmail.com>
2024-11-30 02:46:07 +0300

    feat: add backend URL configuration

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
 
 API.interceptors.request.use(async (config) => {
